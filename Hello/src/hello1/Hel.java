b56bcc1623956fab6b639715cc078f49e3c69958
KGITBank <KGITBank@DESKTOP-UV3DQ4N>
2020-03-04 17:17:41 +0900

    하하하하하하

---
+++ Hello/src/hello1/Hel.java
@@ -0,0 +1,12 @@
+package hello1;
+
+public class Hel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("하이하이!!");
+		System.out.println("헤헤헤헤헤!!");
+		
+	}
+
+}
