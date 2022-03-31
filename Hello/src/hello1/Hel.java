1e9582b05a97102f0a1a14bed28be7b9b92d991a
Yoon Kim <ep2525@naver.com>
2022-03-31 22:59:47 +0900

    Revert "Delete Hello directory"
    This reverts commit 1eb3d63c1f2419a098661828c0e5a365d8113037.

---
+++ Hello/src/hello1/Hel.java
@@ -0,0 +1,13 @@
+package hello1;
+
+public class Hel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("하이하이!!");
+		System.out.println("헤헤헤헤헤!!");
+		System.out.println("룽메룽메!!");
+		
+	}
+
+}
